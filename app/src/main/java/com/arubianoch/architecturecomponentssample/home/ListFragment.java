974d0a31d6b568085d920027b3c1ef7fe8e07b6b
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-02-15 01:16:19 -0500

    Added observer in fragment from ViewModel class

--- app/src/main/java/com/arubianoch/architecturecomponentssample/home/ListFragment.java
+++ app/src/main/java/com/arubianoch/architecturecomponentssample/home/ListFragment.java
@@ -1,5 +1,6 @@
 package com.arubianoch.architecturecomponentssample.home;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -19,6 +20,7 @@ public class ListFragment extends Fragment {
     private RecyclerView listView;
     private TextView errorTextView;
     private View loadingView;
+    private ListViewModel viewModel;
 
     @Nullable
     @Override
@@ -31,5 +33,35 @@ public class ListFragment extends Fragment {
         loadingView = view.findViewById(R.id.loading_view);
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel.class);
+        observeViewModel();
+    }
+
+    private void observeViewModel() {
+        viewModel.getRepos().observe(this, repos -> {
+            if (repos != null) {
+                listView.setVisibility(View.VISIBLE);
+            }
+        });
+        viewModel.getRepoLoadError().observe(this, isError -> {
+            if (isError) {
+                errorTextView.setVisibility(View.VISIBLE);
+                listView.setVisibility(View.GONE);
+                errorTextView.setText(R.string.api_error_response);
+            }
+        });
+        viewModel.getLoading().observe(this, isLoading -> {
+            loadingView.setVisibility(isLoading? View.VISIBLE : View.GONE);
+            if (isLoading) {
+                errorTextView.setVisibility(View.GONE);
+                listView.setVisibility(View.GONE);
+            }
+        });
     }
 }
