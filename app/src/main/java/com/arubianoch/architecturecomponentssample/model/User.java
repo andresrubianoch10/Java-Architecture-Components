7177b56f4113fb9c502c4b49d684b7f6f24bb822
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-02-15 00:45:39 -0500

    Adding User and Repo models

---
+++ app/src/main/java/com/arubianoch/architecturecomponentssample/model/User.java
@@ -0,0 +1,13 @@
+package com.arubianoch.architecturecomponentssample.model;
+
+/**
+ * @author Andres Rubiano Del Chiaro
+ */
+public class User {
+
+    public final String login;
+
+    public User(String login) {
+        this.login = login;
+    }
+}
