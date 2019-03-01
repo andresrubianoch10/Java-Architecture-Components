2e0639a6fac65960dd8c10bfb5f521ada087ae01
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-02-28 23:46:47 -0500

    Created static field inside Adapter class.

--- app/src/main/java/com/arubianoch/architecturecomponentssample/home/RepoListAdapter.java
+++ app/src/main/java/com/arubianoch/architecturecomponentssample/home/RepoListAdapter.java
@@ -1,13 +1,50 @@
 package com.arubianoch.architecturecomponentssample.home;
 
+import android.arch.lifecycle.LifecycleOwner;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.arubianoch.architecturecomponentssample.R;
+import com.arubianoch.architecturecomponentssample.model.Repo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Andres Rubiano Del Chiaro
  */
-public class RepoListAdapter {
+public class RepoListAdapter extends RecyclerView.Adapter<RepoListAdapter.RepoViewHolder> {
+
+    private final List<Repo> data = new ArrayList<>();
+
+    public RepoListAdapter(ListViewModel viewModel, LifecycleOwner lifecycleOwner) {
+        viewModel.getRepos().observe(lifecycleOwner, repos -> {
+            data.clear();
+            if (repos != null) {
+                data.addAll(repos);
+                //This validation always should go outside 'if' check.
+                notifyDataSetChanged();//TODO; Use DiffUtils when we have AutoValue models
+            }
+        });
+    }
+
+    @Override
+    public RepoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RepoViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
 
     static final class RepoViewHolder extends RecyclerView.ViewHolder {
 
@@ -19,7 +56,17 @@ public class RepoListAdapter {
         public RepoViewHolder(View itemView) {
             super(itemView);
 
+            repoName = itemView.findViewById(R.id.tv_repo_name);
+            repoDescription = itemView.findViewById(R.id.tv_repo_description);
+            forks = itemView.findViewById(R.id.tv_forks);
+            stars = itemView.findViewWithTag(R.id.tv_stars);
+        }
 
+        void bind(@NonNull Repo repo) {
+            repoName.setText(repo.name);
+            repoDescription.setText(repo.description);
+            forks.setText(String.valueOf(repo.forks));
+            stars.setText(String.valueOf(repo.stars));
         }
     }
 }
